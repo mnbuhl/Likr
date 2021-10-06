f5a0042df5c7cf41f6606fa9f620d3c1e38ad86e
Mikkel Buhl <mikkelnbuhl@gmail.com>
2021-10-06 20:22:48 +0200

    Updated docker compose files to include new services

--- src/Likr.Likes/Startup.cs
+++ src/Likr.Likes/Startup.cs
@@ -1,16 +1,9 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Likr.Likes
 {
@@ -44,7 +37,7 @@ namespace Likr.Likes
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Likr.Likes v1"));
             }
 
-            app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();
 
             app.UseRouting();
 
