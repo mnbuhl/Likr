a1e8eb87fe89e98ffae45881bdf1e860c1629ccd
Mikkel Buhl <mikkelnbuhl@gmail.com>
2021-10-15 00:49:34 +0200

    Added Ocelot API gateway for the backend services

--- src/Gateway/Likr.Gateway/Startup.cs
+++ src/Gateway/Likr.Gateway/Startup.cs
@@ -7,6 +7,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
 
 namespace Likr.Gateway
 {
@@ -16,6 +18,7 @@ namespace Likr.Gateway
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddOcelot();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,15 +29,7 @@ namespace Likr.Gateway
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            });
+            app.UseOcelot().Wait();
         }
     }
 }
