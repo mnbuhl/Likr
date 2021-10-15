6816de13f5c5ed06961b44b2896c0725658639bc
Mikkel Buhl <mikkelnbuhl@gmail.com>
2021-10-15 13:28:26 +0200

    Configured Identity service for Docker & scaffolded necessary pages

--- src/Services/Likr.Identity/Likr.Identity/Startup.cs
+++ src/Services/Likr.Identity/Likr.Identity/Startup.cs
@@ -1,17 +1,11 @@
 using Likr.Identity.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Likr.Identity
 {
@@ -59,10 +53,7 @@ namespace Likr.Identity
             app.UseAuthentication();
             app.UseAuthorization();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapRazorPages();
-            });
+            app.UseEndpoints(endpoints => { endpoints.MapRazorPages(); });
         }
     }
 }
