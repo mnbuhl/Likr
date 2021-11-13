6b0b3b9c2a73e978b29da0deab5b9a743806dced
Mikkel Buhl <mikkelnbuhl@gmail.com>
2021-11-13 14:28:58 +0100

    Enable css purge for smaller css file in production

--- src/Client/Likr/Client/tailwind.config.js
+++ src/Client/Likr/Client/tailwind.config.js
@@ -1,7 +1,7 @@
 const colors = require('tailwindcss/colors');
 module.exports = {
   purge: {
-    enabled: false,
+    enabled: true,
     content: ['./**/*.html', './**/*.razor']
   },
   darkMode: false, // or 'media' or 'class'
