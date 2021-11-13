f6e9a41abd896b424a7830c7beaab75be22b7d4b
Mikkel Buhl <mikkelnbuhl@gmail.com>
2021-11-13 16:32:36 +0100

    Added DatabaseSeeder to Identity Server Service to get synchronized Seed Data across all services

--- src/Services/Likr.Identity/Likr.Identity/Program.cs
+++ src/Services/Likr.Identity/Likr.Identity/Program.cs
@@ -1,11 +1,11 @@
 ﻿using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Likr.Identity.Server.Data;
+using Likr.Identity.Server.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Likr.Identity.Server
 {
@@ -13,7 +13,26 @@ namespace Likr.Identity.Server
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using var scope = host.Services.CreateScope();
+            var services = scope.ServiceProvider;
+            var userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
+
+            try
+            {
+                var seeder = new DatabaseSeeder(userManager);
+                seeder.Seed();
+            }
+            catch (Exception e)
+            {
+                var logger = services.GetRequiredService<ILogger<Program>>();
+                logger.LogError(e, "Error seeding database.");
+            }
+
+            scope.Dispose();
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
