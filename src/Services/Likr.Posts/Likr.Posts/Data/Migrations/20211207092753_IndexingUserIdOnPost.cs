6b4bc8ff12df80c0ba441887c9691b375d15f620
Mikkel Buhl <mikkelnbuhl@gmail.com>
2021-12-07 10:50:46 +0100

    Indexed UserId on Posts for faster search during Profile load

---
+++ src/Services/Likr.Posts/Likr.Posts/Data/Migrations/20211207092753_IndexingUserIdOnPost.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Likr.Posts.Data.Migrations
+{
+    public partial class IndexingUserIdOnPost : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // See designer file for changes to this migration
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
